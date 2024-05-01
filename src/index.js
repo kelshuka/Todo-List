5a60c94668a5f618149428c457a35ddc741d8d74
Ukam <ukam@Xubuntu.myguest.virtualbox.org>
2024-05-01 22:10:20 +0000

    Add .gitignore and package-lock.json and package.json and src and webpack.config.js

---
+++ src/index.js
@@ -0,0 +1,15 @@
+
+import './style.css';
+
+const container = document.querySelector('.container');
+container.classList.add('containerMain');
+
+const heaDing = document.querySelector('.heaDing');
+heaDing.classList.add('header');
+
+const sideBar = document.querySelector('.sideBar');
+sideBar.classList.add('sidebar');
+//sideBar.classList.add('buttns');
+
+const content = document.querySelector('#content');
+content.classList.add('mainBar');
